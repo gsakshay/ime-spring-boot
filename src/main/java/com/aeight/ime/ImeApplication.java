9c1ef08b6cfb29340ed63a9ad39b0fff94792794
Akshay Gunjur Surya Prakash <gsakshay20002000@gmail.com>
2024-07-13 15:09:18 -0400

    start

---
+++ src/main/java/com/aeight/ime/ImeApplication.java
@@ -0,0 +1,13 @@
+package com.aeight.ime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aeight/ime/ImeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeight.ime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
